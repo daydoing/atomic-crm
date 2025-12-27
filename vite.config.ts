dba7f1686836b10bcf0358320cb37d9a8a21403e
Jay <106968807+ethelbell@users.noreply.github.com>
2025-12-27 10:26:45 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import path from "node:path";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
+import createHtmlPlugin from "vite-plugin-simple-html";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      open: process.env.NODE_ENV !== "CI",
+      filename: "./dist/stats.html",
+    }),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          mainScript: `src/main.tsx`,
+        },
+      },
+    }),
+  ],
+  base: "./",
+  esbuild: {
+    keepNames: true,
+  },
+  build: {
+    sourcemap: true,
+  },
+  resolve: {
+    preserveSymlinks: true,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
